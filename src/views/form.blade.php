7ef15283731ba114424ce3c58ad5b94d196ea62d
Muhammad Zamroni <halo@matriphe.com>
2015-06-11 21:29:44 +0700

    Add CSRF Token on test view.

--- src/views/form.blade.php
+++ src/views/form.blade.php
@@ -13,6 +13,7 @@
             <input type="text" name="gcm_id" placeholder="GCM ID"><br>
             <input type="text" name="subject" placeholder="Subject"><br>
             <input type="text" name="message" placeholder="Message"><br>
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
             <button type="submit">Send!</button>
         </form>
     </body>
